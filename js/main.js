cba42b2129b6e39e5c76a57728ab525485aed72b
davidearbola <davidearbola@gmail.com>
2024-04-30 15:58:20 +0200

    M2 - inietto i quadrati nell'html tramite ciclo js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// MILESTONE #2
+// Commentate l'HTML e iniettatelo tramite un ciclo in JS, senza pensare al click o alle funzioni.
+
+// seleziono il div grid che conterrà le colonne
+let grid = document.getElementById(`grid`);
+// ciclo for
+for (let i = 1; i <= 100; i++) {
+	// ad ogni ciclo creo un elemento e lo inserisco nel div grid e gli aggiungo la classe .col
+	let elementoDiv = document.createElement(`div`);
+	// inserisco del testo dentro al div creato
+	elementoDiv.innerText = `${i}`;
+	// inserisco il div creato in grid
+	grid.append(elementoDiv);
+	// aggiungo la classe
+	elementoDiv.classList.add(`col`);
+}
