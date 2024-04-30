d3482f221a6839e538bb6f4ffdcb84f0528ebac3
davidearbola <davidearbola@gmail.com>
2024-04-30 17:22:03 +0200

    Bonus3 - creo select e in base alla scelta richiama la funziona che crea una griglia diversa per la difficolta 10col-9col-7col

--- js/main.js
+++ js/main.js
@@ -75,21 +75,101 @@
 // BONUS #2: CAPIRE EVENTI E FUNZIONI
 // Provate a eseguire quanto fatto fin qui NON al caricamento della pagina, ma al click su un pulsante "Avvia gioco".
 
+// // seleziono il div grid che conterrà le colonne
+// let grid = document.getElementById(`grid`);
+// // creo variabili da usare come paramentri nella funzione
+// let div = `div`;
+// let classeCol = `col`;
+// let classeColore = `bg`;
+
+// // seleziono bottone
+// let btnStart = document.getElementById(`start`);
+// // aggiungo eventlistener
+// btnStart.addEventListener(`click`, function () {
+// 	// ciclo for
+// 	for (let i = 1; i <= 100; i++) {
+// 		// ad ogni click viene richiamata funzione che crea una colonna con numero e la colora
+// 		let elementoDiv = creaColonna(i, grid, div, classeCol, classeColore);
+// 	}
+// });
+
+// // funzione per creare colonna dentro ad un div e ne scrive il numero dentro ad un ciclo ed al click viene colorata o tolto il colore//
+// function creaColonna(numero, griglia, element, classe1, classe2) {
+// 	let elementoDiv = document.createElement(element);
+// 	elementoDiv.innerText = `${numero}`;
+// 	griglia.append(elementoDiv);
+// 	elementoDiv.classList.add(classe1);
+// 	// aggiungo evento di click
+// 	elementoDiv.addEventListener(`click`, function () {
+// 		// aggiungo o rimuovo classe per colore di sfondo colonna
+// 		elementoDiv.classList.toggle(classe2);
+// 		console.log(`Hai cliccato sulla casella numero: ${numero}`);
+// 	});
+// 	return elementoDiv;
+// }
+
+// BONUS #3: QUANDO IL GIOCO SI FA DURO
+// Aggiungere una select accanto al bottone di start. La select presenterà tre difficoltà di gioco tra cui scegliere (easy, medium, hard).
+// Quando generate la griglia userete un numero di celle che dipende dalla difficoltà.
+// Easy: 100 celle (10x10)
+// Medium: 81 celle (9x9)
+// Hard: 49 (7x7)
+// //***************************************************** */
 // seleziono il div grid che conterrà le colonne
 let grid = document.getElementById(`grid`);
 // creo variabili da usare come paramentri nella funzione
 let div = `div`;
-let classeCol = `col`;
+let classeCol10 = `col-10`;
+let classeCol9 = `col-9`;
+let classeCol7 = `col-7`;
 let classeColore = `bg`;
 
 // seleziono bottone
 let btnStart = document.getElementById(`start`);
 // aggiungo eventlistener
 btnStart.addEventListener(`click`, function () {
-	// ciclo for
-	for (let i = 1; i <= 100; i++) {
-		// ad ogni click viene richiamata funzione che crea una colonna con numero e la colora
-		let elementoDiv = creaColonna(i, grid, div, classeCol, classeColore);
+	// prendo il valore della select
+	let valoreSelect = document.getElementById(`difficulty`).value;
+	// if per controllare il valore delle select in base al valore farò dei cicli con parametri diversi
+	if (valoreSelect == `easy`) {
+		for (let i = 1; i <= 100; i++) {
+			// aggiungo classe per width a grid
+			grid.classList.add(`w-1000`);
+			// ad ogni click viene richiamata funzione che crea una colonna con numero e la colora
+			let elementoDiv = creaColonna(
+				i,
+				grid,
+				div,
+				classeCol10,
+				classeColore
+			);
+		}
+	} else if (valoreSelect == `medium`) {
+		for (let i = 1; i <= 81; i++) {
+			// aggiungo classe per width a grid
+			grid.classList.add(`w-900`);
+			// ad ogni click viene richiamata funzione che crea una colonna con numero e la colora
+			let elementoDiv = creaColonna(
+				i,
+				grid,
+				div,
+				classeCol9,
+				classeColore
+			);
+		}
+	} else if (valoreSelect == `hard`) {
+		for (let i = 1; i <= 49; i++) {
+			// aggiungo classe per width a grid
+			grid.classList.add(`w-700`);
+			// ad ogni click viene richiamata funzione che crea una colonna con numero e la colora
+			let elementoDiv = creaColonna(
+				i,
+				grid,
+				div,
+				classeCol7,
+				classeColore
+			);
+		}
 	}
 });
 
